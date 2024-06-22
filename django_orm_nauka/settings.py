3977b07005331c606782325be2f2f751a8a8eabd
damian krzystolik <damiankrzystolik@gmail.com>
2024-06-22 19:55:46 +0200

    .

--- django_orm_nauka/settings.py
+++ django_orm_nauka/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-307874*m&k2)(dy+%$5h#a0df&mv-rako0*gy!16%@3kkkw-k5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
